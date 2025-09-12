bd9290323537467f29f84b67bae38d1a60832c93
Manuel Lopez <jmlopez.rod@gmail.com>
2025-09-12 18:58:54 -0500

    use pkglink

--- tasks/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-if [[ ! -f 'uv.lock' ]]; then
-    uv lock
-fi
-
-installOptions=${UV_INSTALL_OPTS:='--group dev --all-extras --locked'}
-uv sync $installOptions
--- tasks/symlinks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-ln -fs .codeguide/config/Makefile Makefile
-ln -fs .codeguide/config/.editorconfig .editorconfig
